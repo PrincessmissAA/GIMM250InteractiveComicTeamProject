09e75e945379922f65db8d18e1b8e34b28e12579
PrincessmissAA <73598180+PrincessmissAA@users.noreply.github.com>
2024-04-29 14:18:52 -0600

    Added scene change from panels to maze game

--- Assets/ComicPanels/SceneChangeComic.cs
+++ Assets/ComicPanels/SceneChangeComic.cs
@@ -5,11 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class SceneChangeComic : MonoBehaviour
 {
-    //public int[] comicPanel = new int[25];
-    // Start is called before the first frame update
-
-
-
+    
     void Start()
     {
         
